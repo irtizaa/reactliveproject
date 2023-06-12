c82b64cef0be6e5813035dd4464712bf5fe2f89b
irtizaa <irtizahussain.edu098@gmail.com>
2023-06-12 12:28:54 +0500

    Updates

---
+++ precache-manifest.69e9cb1eff7095de3d80ec0b21b6f49e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e416b786d967d41e1989",
+    "url": "/reactliveproject/static/css/main.7a9700f5.chunk.css"
+  },
+  {
+    "revision": "e416b786d967d41e1989",
+    "url": "/reactliveproject/static/js/main.863ea2f0.chunk.js"
+  },
+  {
+    "revision": "3a736068f8fbb896d112",
+    "url": "/reactliveproject/static/js/runtime~main.7c756870.js"
+  },
+  {
+    "revision": "cf253aab01ffe4d0f7f3",
+    "url": "/reactliveproject/static/css/2.38df1df8.chunk.css"
+  },
+  {
+    "revision": "cf253aab01ffe4d0f7f3",
+    "url": "/reactliveproject/static/js/2.379e162b.chunk.js"
+  },
+  {
+    "revision": "06cf098334c266479f5bd9498faaf07c",
+    "url": "/reactliveproject/index.html"
+  }
+];
